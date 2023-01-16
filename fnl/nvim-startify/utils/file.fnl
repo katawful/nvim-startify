f9ff97e3d33f4993f163f8c5226fc78a719734fa
katawful <katisntgood@gmail.com>
2023-01-15 21:15:13 -0500

    [utils.file] add oldfile manager

--- fnl/nvim-startify/utils/file.fnl
+++ fnl/nvim-startify/utils/file.fnl
@@ -1,6 +1,21 @@
-(module nvim-startify.utils.file)
+(module nvim-startify.utils.file
+        {autoload {a nvim-startify.aniseed.core}
+         require-macros [nvim-startify.katcros-fnl.macros.nvim.api.options.macros]})
 
 ;;; Module: file management
 
+(defn- remove-from-seq-tbl [seq key] "Removes a key from a sequential table"
+       (let [output []]
+         (each [_ val (ipairs seq)]
+           (if (not= key val)
+             (table.insert output val)))
+         output))
+
 (defn update-oldfiles [file] "Updates oldfiles with current file"
-      (print file "OLDFILES UPDATED"))
+      (if (or (= (get-var :g :startify_locked) 0)
+              (= (get-var :g :startify_locked) nil)
+              (do-viml exists "v:oldfiles"))
+        (do
+          (remove-from-seq-tbl (get-var :v :oldfiles) file)
+          (table.insert (get-var :v :oldfiles) 0 file))))
+
--- lua/nvim-startify/utils/file.lua
+++ lua/nvim-startify/utils/file.lua
@@ -10,8 +10,28 @@ do
   _2amodule_2a["aniseed/locals"] = {}
   _2amodule_locals_2a = (_2amodule_2a)["aniseed/locals"]
 end
+local autoload = (require("nvim-startify.aniseed.autoload")).autoload
+local a, _ = autoload("nvim-startify.aniseed.core"), nil
+_2amodule_locals_2a["a"] = a
+_2amodule_locals_2a["_"] = _
+local function remove_from_seq_tbl(seq, key)
+  local output = {}
+  for _0, val in ipairs(seq) do
+    if (key ~= val) then
+      table.insert(output, val)
+    else
+    end
+  end
+  return output
+end
+_2amodule_locals_2a["remove-from-seq-tbl"] = remove_from_seq_tbl
 local function update_oldfiles(file)
-  return print(file, "OLDFILES UPDATED")
+  if ((vim.g.startify_locked == 0) or (vim.g.startify_locked == nil) or __fnl_global__do_2dviml(exists, "v:oldfiles")) then
+    remove_from_seq_tbl(vim.v.oldfiles, file)
+    return table.insert(vim.v.oldfiles, 0, file)
+  else
+    return nil
+  end
 end
 _2amodule_2a["update-oldfiles"] = update_oldfiles
 return _2amodule_2a
