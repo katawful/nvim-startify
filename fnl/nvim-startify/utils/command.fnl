484815d0c2b5705a8b66e90a0418229d9e98e23b
katawful <katisntgood@gmail.com>
2023-01-22 21:53:20 -0500

    [utils.command] add user commands

--- fnl/nvim-startify/utils/command.fnl
+++ fnl/nvim-startify/utils/command.fnl
@@ -1,6 +1,30 @@
-(module nvim-startify.utils.command)
-        ; {require-macros [katcros-fnl.macros.nvim.api.utils.macros]})
+(module nvim-startify.utils.command
+        {autoload {session-load nvim-startify.utils.session.loader
+                   session-write nvim-startify.utils.session.write}
+         require-macros [nvim-startify.katcros-fnl.macros.nvim.api.utils.macros]})
 
 ;;; Module: generate and run user-commands
 
-; (defn init [] "Initialization")
+(defn completion-sload [] "Completion for user command 'SLoad'"
+      {})
+(defn completion-ssave [] "Completion for user command 'SSave'"
+      {})
+(defn completion-sdelete [] "Completion for user command 'SDelete'"
+      {})
+
+(defn init [] "Initialization"
+      (cre-command :SLoad (fn [args] (session-load.init args.bang args.fargs))
+                          {:nargs "?"
+                           :bang true
+                           :bar true
+                           :complete completion-sload})
+      (cre-command :SSave (fn [args] (session-write.save args.bang args.fargs))
+                          {:nargs "?"
+                           :bang true
+                           :bar true
+                           :complete completion-ssave})
+      (cre-command :SDelete (fn [args] (session-delete.init args.bang args.fargs))
+                            {:nargs "?"
+                             :bang true
+                             :bar true
+                             :complete completion-sdelete}))
--- lua/nvim-startify/utils/command.lua
+++ lua/nvim-startify/utils/command.lua
@@ -10,3 +10,36 @@ do
   _2amodule_2a["aniseed/locals"] = {}
   _2amodule_locals_2a = (_2amodule_2a)["aniseed/locals"]
 end
+local autoload = (require("nvim-startify.aniseed.autoload")).autoload
+local session_load, session_write, _ = autoload("nvim-startify.utils.session.loader"), autoload("nvim-startify.utils.session.write"), nil
+_2amodule_locals_2a["session-load"] = session_load
+_2amodule_locals_2a["session-write"] = session_write
+_2amodule_locals_2a["_"] = _
+local function completion_sload()
+  return {}
+end
+_2amodule_2a["completion-sload"] = completion_sload
+local function completion_ssave()
+  return {}
+end
+_2amodule_2a["completion-ssave"] = completion_ssave
+local function completion_sdelete()
+  return {}
+end
+_2amodule_2a["completion-sdelete"] = completion_sdelete
+local function init()
+  local function _1_(args)
+    return session_load.init(args.bang, args.fargs)
+  end
+  vim.api.nvim_create_user_command("SLoad", _1_, {nargs = "?", bang = true, bar = true, complete = completion_sload})
+  local function _2_(args)
+    return session_write.save(args.bang, args.fargs)
+  end
+  vim.api.nvim_create_user_command("SSave", _2_, {nargs = "?", bang = true, bar = true, complete = completion_ssave})
+  local function _3_(args)
+    return __fnl_global__session_2ddelete.init(args.bang, args.fargs)
+  end
+  return vim.api.nvim_create_user_command("SDelete", _3_, {nargs = "?", bang = true, bar = true, complete = completion_sdelete})
+end
+_2amodule_2a["init"] = init
+return _2amodule_2a
