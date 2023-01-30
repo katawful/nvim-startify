01b9343ca9ef57571cc2743a0feec98a412e7a16
katawful <katisntgood@gmail.com>
2023-01-29 19:44:58 -0500

    [utils.file] add startify variable and lastline

--- fnl/nvim-startify/utils/file.fnl
+++ fnl/nvim-startify/utils/file.fnl
@@ -1,8 +1,13 @@
 (module nvim-startify.utils.file
         {autoload {a nvim-startify.aniseed.core}
-         require-macros [nvim-startify.katcros-fnl.macros.nvim.api.options.macros]})
+         require-macros [nvim-startify.katcros-fnl.macros.nvim.api.options.macros
+                         nvim-startify.katcros-fnl.macros.nvim.api.utils.macros]})
 
 ;;; Module: file management
+
+;; Key-val: stores information for startify page
+;; TODO: I eventually want to expand this to unique startify pages
+(def startify {:lastline -1})
 
 (defn- remove-from-seq-tbl [seq key] "Removes a key from a sequential table"
        (let [output []]
@@ -19,3 +24,7 @@
           (remove-from-seq-tbl (get-var :v :oldfiles) file)
           (table.insert (get-var :v :oldfiles) 0 file))))
 
+(defn lastline [buffer] "Returns the last line of startify page"
+      (length (vim.api.nvim_buf_get_lines buffer 0 -1 false)))
+
+(def separator (if (do-viml has :win32) "\\" "/"))
--- lua/nvim-startify/utils/file.lua
+++ lua/nvim-startify/utils/file.lua
@@ -11,12 +11,15 @@ do
   _2amodule_locals_2a = (_2amodule_2a)["aniseed/locals"]
 end
 local autoload = (require("nvim-startify.aniseed.autoload")).autoload
-local a, _ = autoload("nvim-startify.aniseed.core"), nil
+local a, _, _0 = autoload("nvim-startify.aniseed.core"), nil, nil
 _2amodule_locals_2a["a"] = a
-_2amodule_locals_2a["_"] = _
+_2amodule_locals_2a["_"] = _0
+_2amodule_locals_2a["_"] = _0
+local startify = {lastline = -1}
+_2amodule_2a["startify"] = startify
 local function remove_from_seq_tbl(seq, key)
   local output = {}
-  for _0, val in ipairs(seq) do
+  for _1, val in ipairs(seq) do
     if (key ~= val) then
       table.insert(output, val)
     else
@@ -26,7 +29,15 @@ local function remove_from_seq_tbl(seq, key)
 end
 _2amodule_locals_2a["remove-from-seq-tbl"] = remove_from_seq_tbl
 local function update_oldfiles(file)
-  if ((vim.g.startify_locked == 0) or (vim.g.startify_locked == nil) or __fnl_global__do_2dviml(exists, "v:oldfiles")) then
+  local function _2_()
+    local result_2_auto = vim.fn.exists("v:oldfiles")
+    if (result_2_auto == 0) then
+      return false
+    else
+      return true
+    end
+  end
+  if ((vim.g.startify_locked == 0) or (vim.g.startify_locked == nil) or _2_()) then
     remove_from_seq_tbl(vim.v.oldfiles, file)
     return table.insert(vim.v.oldfiles, 0, file)
   else
@@ -34,4 +45,24 @@ local function update_oldfiles(file)
   end
 end
 _2amodule_2a["update-oldfiles"] = update_oldfiles
+local function lastline(buffer)
+  return #vim.api.nvim_buf_get_lines(buffer, 0, -1, false)
+end
+_2amodule_2a["lastline"] = lastline
+local separator
+local _5_
+do
+  local result_2_auto = vim.fn.has("win32")
+  if (result_2_auto == 0) then
+    _5_ = false
+  else
+    _5_ = true
+  end
+end
+if _5_ then
+  separator = "\\"
+else
+  separator = "/"
+end
+_2amodule_2a["separator"] = separator
 return _2amodule_2a
